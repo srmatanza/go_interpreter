082b75a6a7762a7c2255b7f44afa2ceb19fc81da
Mike McEuen <srmatanza@gmail.com>
2022-07-03 12:16:21 -0400

    add repl

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/user"
+
+	"github.com/srmatanza/go_interpreter/repl"
+)
 
 func main() {
-	fmt.Println("hello there!")
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s! This is the Monkey programming language!\n", user.Username)
+	fmt.Printf("Feel free to type in commands\n")
+
+	repl.Start(os.Stdin, os.Stdout)
 }
